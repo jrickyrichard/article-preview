8a4c4f76648f191e2259b93dca4c1ab9cc6c49e2
jrickyrichard <jrrsum1010@gmail.com>
2023-03-28 10:41:17 +0800

    small fix

--- public/script.js
+++ public/script.js
@@ -1,9 +1,5 @@
 
 document.querySelector('#share').addEventListener('click', () => {
-    document.querySelector('.pop').classList.remove('invisible');
+    document.querySelector('.pop').classList.toggle('invisible');
 });
 
-// hide pop-up when mouse is out of icon and pop-up
-// document.querySelector('.m-8').addEventListener('click', () => {
-//     document.querySelector('.pop').classList.add('invisible');
-// });
